cfd7f7d32453f70bbad11f62e41ca8ed4bd9a824
Systeme Oblivionne <122670261+Beeckaert@users.noreply.github.com>
2024-06-14 00:16:47 +0200

    14 juin 2024

---
+++ SiteVitrinePetiteChouette1/Js/Anim/Menu/HeadMenu.js
@@ -0,0 +1,99 @@
+// Dev By SystemeRubis
+
+// Variable Get Id
+
+// Variable Recuperation Div Head Menu
+
+const DivAP = document.getElementById("DivLinkAP");
+const DivContact = document.getElementById("DivLinkContact");
+const DivCreation = document.getElementById("DivLinkCreation");
+const DivBlog = document.getElementById("DivLinkBlog");
+
+// Variable Recuperation Link Head Menu
+
+const LinkAP = document.getElementById("LinkAP");
+const LinkContact = document.getElementById("LinkContact");
+const LinkCreation = document.getElementById("LinkCreation");
+const LinkBlog = document.getElementById("LinkBlog");
+
+// Start Anim
+
+// Link Avant-Propos
+
+LinkAP.addEventListener("mouseenter", function(){
+
+    LinkAP.style.textDecoration ="underline";
+
+})
+
+LinkAP.addEventListener("mousemove", function(){
+
+    LinkAP.style.textDecoration ="underline";
+
+})
+
+LinkAP.addEventListener("mouseout", function(){
+
+    LinkAP.style.textDecoration ="none";
+
+})
+
+// Link Contact
+
+LinkContact.addEventListener("mouseenter", function(){
+
+    LinkContact.style.textDecoration ="underline";
+
+})
+
+LinkContact.addEventListener("mousemove", function(){
+
+    LinkContact.style.textDecoration ="underline";
+
+})
+
+LinkContact.addEventListener("mouseout", function(){
+
+    LinkContact.style.textDecoration ="none";
+
+})
+
+// Link Creation
+
+LinkCreation.addEventListener("mouseenter", function(){
+
+    LinkCreation.style.textDecoration ="underline";
+
+})
+
+LinkCreation.addEventListener("mousemove", function(){
+
+    LinkCreation.style.textDecoration ="underline";
+
+})
+
+LinkCreation.addEventListener("mouseout", function(){
+
+    LinkCreation.style.textDecoration ="none";
+
+})
+
+// Link Blog
+
+LinkBlog.addEventListener("mouseenter", function(){
+
+    LinkBlog.style.textDecoration ="underline";
+
+})
+
+LinkBlog.addEventListener("mousemove", function(){
+
+    LinkBlog.style.textDecoration ="underline";
+
+})
+
+LinkBlog.addEventListener("mouseout", function(){
+
+    LinkBlog.style.textDecoration ="none";
+
+})
